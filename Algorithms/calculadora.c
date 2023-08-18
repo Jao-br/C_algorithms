1ecbf0b1d9741295c432162747291b57c1fc5231
JOÃO CARLOS CASTRO MENDES DE LIMA <joao.clima@a.ucb.br>
2023-08-18 09:59:47 -0300

    Add 'calculadora.c'

---
+++ Algorithms/calculadora.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+    int numero1 = 0;
+    int numero2 = 0;
+    int resultado = 0;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &numero1);
+    
+
+    resultado = numero1 + numero2;
+
+
+
+
+
+     
+}
