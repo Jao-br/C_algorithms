5418e2befcd1960cfaef50bbf8552c82b87f2f9e
JOÃO CARLOS CASTRO MENDES DE LIMA <joao.clima@a.ucb.br>
2023-08-18 10:08:47 -0300

    Add new functions to 'calculadora.c'

--- Algorithms/calculadora.c
+++ Algorithms/calculadora.c
@@ -4,12 +4,26 @@
 int main(){
     int numero1;
     int numero2;
-    int resultado;
+    int soma, subtracao, multiplicacao, divisao, resto;
 
     numero1 = 10;
     numero2 = 20;
 
-    resultado = numero1 + numero2;
+    soma = numero1 + numero2;
     printf("O Resultado da soma é: %d\n", resultado);
+
+    subtracao = numero1 - numero2;
+    printf("O Resultado da subtração é: %d\n", resultado);
+
+    multiplicacao = numero1 * numero2;
+    printf("O Resultado da multiplicação é: %d\n", resultado);
+
+    divisao = numero1 / numero2;
+    printf("O Resultado da divisão é: %d\n", resultado);
+
+    resto = numero1 % numero2;
+    printf("O Resultado do resto é: %d\n", resultado);
+
+    return 0;
 
 }
